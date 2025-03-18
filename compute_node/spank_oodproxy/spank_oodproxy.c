7a069f72cc83057d4020eb2730698052d300c2ea
Ryan Cox <ryan_cox@byu.edu>
2025-03-18 14:25:22 -0600

    spacing

--- compute_node/spank_oodproxy/spank_oodproxy.c
+++ compute_node/spank_oodproxy/spank_oodproxy.c
@@ -1,26 +1,26 @@
 /*
-* Author: Ryan Cox
-* 
-* Copyright (C) 2025, Brigham Young University
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, see
-* <https://www.gnu.org/licenses/>.
-*
-*
-* Compile with: gcc -I/usr/local/src/slurm -fPIC -shared -lcap -o spank_oodproxy{.so,.c}
-*
-*/
+ * Author: Ryan Cox
+ * 
+ * Copyright (C) 2025, Brigham Young University
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Compile with: gcc -I/usr/local/src/slurm -fPIC -shared -lcap -o spank_oodproxy{.so,.c}
+ *
+ */
 
 #define _GNU_SOURCE
 #include <sched.h>
